62f7c775c195ba572f9f91a4b780f364f18e1de9
Tim Boehning <t.boehning@web.de>
2020-09-22 21:54:50 +0200

    Added proper testing values and moved them to Misc.h

---
+++ JobOperationOptimizer/Misc.h
@@ -0,0 +1,58 @@
+#include "JobList.h"
+
+namespace Misc {
+	void fill_first_job(Job &job) {
+		const int JOB_NUMBER = job.number;
+
+		job.append_operation(JobOperation("Schruppen", JOB_NUMBER, 1, 1, Vector(0.0, 0.0, 0.0), Vector(10.3, 12.0, 5.64)));
+	}
+
+	void fill_second_job(Job &job) {
+		const int JOB_NUMBER = job.number;
+
+		job.append_operation(JobOperation("Schlichten", JOB_NUMBER, 1, 2, Vector(100.3, 0.0, 0.0), Vector(10.3, 12.0, 5.64)));
+	}
+
+	void fill_third_job(Job &job) {
+		const int JOB_NUMBER = job.number;
+
+		job.append_operation(JobOperation("Bohren", JOB_NUMBER, 1, 3, Vector(0.0, 0.0, 0.0), Vector(10.3, 12.0, 5.64)));
+		job.append_operation(JobOperation("Gewindebohren 1", JOB_NUMBER, 2, 4, Vector(30.12, 10.1, 200.3), Vector(10.3, 12.0, 5.64)));
+		job.append_operation(JobOperation("Gewindebohren 2", JOB_NUMBER, 2, 5, Vector(30.12, 10.1, 200.3), Vector(10.3, 12.0, 5.64)));
+	}
+
+	void fill_fourth_job(Job &job) {
+		const int JOB_NUMBER = job.number;
+
+		job.append_operation(JobOperation("Bohren", JOB_NUMBER, 1, 3, Vector(0.0, 0.0, 0.0), Vector(10.3, 12.0, 5.64)));
+		job.append_operation(JobOperation("Gewindebohren 1", JOB_NUMBER, 2, 4, Vector(30.12, 10.1, 200.3), Vector(10.3, 12.0, 5.64)));
+		job.append_operation(JobOperation("Gewindebohren 2", JOB_NUMBER, 2, 6, Vector(30.12, 10.1, 200.3), Vector(10.3, 12.0, 5.64)));
+	}
+
+	void fill_first_job_length_zero(Job &job) {
+		const int JOB_NUMBER = job.number;
+
+		job.append_operation(JobOperation("TEST", JOB_NUMBER, 1, 1, Vector(0.0, 0.0, 0.0), Vector(10.3, 12.0, 5.64)));
+		job.append_operation(JobOperation("TEST", JOB_NUMBER, 2, 2, Vector(42.2, 52.123, 68.0), Vector(30.12, 10.1, 200.3)));
+		job.append_operation(JobOperation("TEST", JOB_NUMBER, 3, 3, Vector(100.0, 200.0, 300.0), Vector(20.11, 54.12, 0.0)));
+		job.append_operation(JobOperation("TEST", JOB_NUMBER, 4, 4, Vector(321.3, 142.1, 400.0), Vector(0.21, 532.1, 345.14)));
+	}
+
+	void fill_second_job_length_zero(Job &job) {
+		const int JOB_NUMBER = job.number;
+
+		job.append_operation(JobOperation("TEST", JOB_NUMBER, 1, 1, Vector(10.3, 12.0, 5.64), Vector(15.8, 65.1, 14.21)));
+		job.append_operation(JobOperation("TEST", JOB_NUMBER, 2, 2, Vector(30.12, 10.1, 200.3), Vector(20.3, 43.0, 10.64)));
+		job.append_operation(JobOperation("TEST", JOB_NUMBER, 3, 3, Vector(20.11, 54.12, 0.0), Vector(11.3, 112.0, 45.64)));
+		job.append_operation(JobOperation("TEST", JOB_NUMBER, 4, 4, Vector(0.21, 532.1, 345.14), Vector(43.3, 1.0, 2112.64)));
+	}
+
+	void fill_third_job_length_zero(Job &job) {
+		const int JOB_NUMBER = job.number;
+
+		job.append_operation(JobOperation("TEST", JOB_NUMBER, 1, 1, Vector(15.8, 65.1, 14.21), Vector(42.2, 52.123, 68.0)));
+		job.append_operation(JobOperation("TEST", JOB_NUMBER, 2, 2, Vector(20.3, 43.0, 10.64), Vector(100.0, 200.0, 300.0)));
+		job.append_operation(JobOperation("TEST", JOB_NUMBER, 3, 3, Vector(11.3, 112.0, 45.64), Vector(15.3, 12.0, 5.64)));
+		job.append_operation(JobOperation("TEST",JOB_NUMBER, 4, 4, Vector(43.3, 1.0, 2112.64), Vector(41.3, 87.2, 1000.4)));
+	}
+}
--- JobOperationOptimizer/main.cpp
+++ JobOperationOptimizer/main.cpp
@@ -1,72 +1,36 @@
 #include "JobList.h"
 #include "Permutation.h"
-
-void fill_first_job(Job &job) {
-	const int JOB_NUMBER = job.number;
-
-	job.append_operation(JobOperation("Bohren", JOB_NUMBER, 1, 1, Vector(0.0, 0.0, 0.0), Vector(10.3, 12.0, 5.64)));
-	job.append_operation(JobOperation("Bohren", JOB_NUMBER, 2, 1, Vector(20.1, 100.3, 50.0), Vector(30.12, 10.1, 200.3)));
-	job.append_operation(JobOperation("Bohren", JOB_NUMBER, 3, 3, Vector(30.12, 100.3, 0.0), Vector(10.3, 12.0, 5.64)));
-}
-
-void fill_second_job(Job &job) {
-	const int JOB_NUMBER = job.number;
-
-	job.append_operation(JobOperation("Bohren", JOB_NUMBER, 1, 2, Vector(100.3, 0.0, 0.0), Vector(10.3, 12.0, 5.64)));
-	job.append_operation(JobOperation("Bohren", JOB_NUMBER, 2, 3, Vector(400.0, 500.0, 600.0), Vector(20.3, 43.0, 10.64)));
-	job.append_operation(JobOperation("Bohren", JOB_NUMBER, 3, 1, Vector(10.3, 12.0, 5.0), Vector(10.3, 12.0, 5.64)));
-}
-
-void fill_third_job(Job &job) {
-	const int JOB_NUMBER = job.number;
-
-	job.append_operation(JobOperation("Bohren", JOB_NUMBER, 1, 1, Vector(0.0, 0.0, 0.0), Vector(10.3, 12.0, 5.64)));
-	job.append_operation(JobOperation("Bohren", JOB_NUMBER, 2, 3, Vector(30.12, 10.1, 200.3), Vector(10.3, 12.0, 5.64)));
-	job.append_operation(JobOperation("Bohren", JOB_NUMBER, 3, 1, Vector(32.1, 40.2, 0.0), Vector(15.3, 12.0, 5.64)));
-	//job.append_operation(JobOperation(3, 4, 2, Vector(12.0, 312.2, 0.0), Vector(41.3, 87.2, 1000.4))); // Optional
-	//job.append_operation(JobOperation(3, 5, 2, Vector(12.0, 312.2, 0.0), Vector(41.3, 87.2, 1000.4))); // Optional
-}
-
-void fill_first_job_length_zero(Job &job) {
-	const int JOB_NUMBER = job.number;
-
-	job.append_operation(JobOperation("Bohren", JOB_NUMBER, 1, 1, Vector(0.0, 0.0, 0.0), Vector(10.3, 12.0, 5.64)));
-	job.append_operation(JobOperation("Bohren", JOB_NUMBER, 2, 1, Vector(42.2, 52.123, 68.0), Vector(30.12, 10.1, 200.3)));
-	job.append_operation(JobOperation("Bohren", JOB_NUMBER, 3, 3, Vector(100.0, 200.0, 300.0), Vector(20.11, 54.12, 0.0)));
-}
-
-void fill_second_job_length_zero(Job &job) {
-	const int JOB_NUMBER = job.number;
-
-	job.append_operation(JobOperation("Bohren", JOB_NUMBER, 1, 2, Vector(10.3, 12.0, 5.64), Vector(15.8, 65.1, 14.21)));
-	job.append_operation(JobOperation("Bohren", JOB_NUMBER, 2, 3, Vector(30.12, 10.1, 200.3), Vector(20.3, 43.0, 10.64)));
-	job.append_operation(JobOperation("Bohren", JOB_NUMBER, 3, 1, Vector(20.11, 54.12, 0.0), Vector(11.3, 112.0, 45.64)));
-	// job.append_operation(JobOperation("Bohren",JOB_NUMBER, 4, 1, Vector(312.31, 315.1, 54.1), Vector(43.3, 1.0, 2112.64))); // Optional
-}
-
-void fill_third_job_length_zero(Job &job) {
-	const int JOB_NUMBER = job.number;
-
-	job.append_operation(JobOperation("Bohren", JOB_NUMBER, 1, 1, Vector(15.8, 65.1, 14.21), Vector(42.2, 52.123, 68.0)));
-	job.append_operation(JobOperation("Bohren", JOB_NUMBER, 2, 3, Vector(20.3, 43.0, 10.64), Vector(100.0, 200.0, 300.0)));
-	job.append_operation(JobOperation("Bohren", JOB_NUMBER, 3, 1, Vector(11.3, 112.0, 45.64), Vector(15.3, 12.0, 5.64)));
-	// job.append_operation(JobOperation("Bohren",JOB_NUMBER, 4, 2, Vector(12.0, 312.2, 0.0), Vector(41.3, 87.2, 1000.4))); // Optional
-	// job.append_operation(JobOperation("Bohren",JOB_NUMBER, 5, 2, Vector(12.0, 312.2, 0.0), Vector(41.3, 87.2, 1000.4))); // Optional
-}
+#include "Misc.h"
 
 int main() {
-	Job job1(1, "Bohren");
-	Job job2(2, "Bohren");
-	Job job3(3, "Bohren");
+	// Real
+	Job job1(1, "Tasche Schruppen");
+	Job job2(2, "Tasche Schlichten");
+	Job job3(3, "Bohren und Gewinde 1");
+	Job job4(4, "Bohren und Gewinde 2");
 
-	fill_first_job(job1);
-	fill_second_job(job2);
-	fill_third_job(job3);
+	Misc::fill_first_job(job1);
+	Misc::fill_second_job(job2);
+	Misc::fill_third_job(job3);
+	Misc::fill_fourth_job(job4);
 
+	// Real Correct Order for toolchanges: 1.1 -> 2.1 -> 3.1 -> 4.1 -> 3.2 -> 4.2
+
+
+	// Length: 0.0, Changes 4
+	/*Job job1(1, "TEST");
+	Job job2(2, "TEST");
+	Job job3(3, "TEST");
+
+	Misc::fill_first_job_length_zero(job1);
+	Misc::fill_second_job_length_zero(job2);
+	Misc::fill_third_job_length_zero(job3);*/
+ 	
 	JobList allJobs;
 	allJobs.append_job(job1);
 	allJobs.append_job(job2);
 	allJobs.append_job(job3);
+	allJobs.append_job(job4);
 
 	allJobs.print_jobs();
 
