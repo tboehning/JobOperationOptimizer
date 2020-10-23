f8e7ae531221b4bdb47db74fa9f3c3b72fae4a1b
Tim Böhning <t.boehning@web.de>
2020-10-23 13:12:26 +0200

    Refactoring

--- JobOperationOptimizer/JobOperation.h
+++ JobOperationOptimizer/JobOperation.h
@@ -10,19 +10,20 @@ class JobOperation
 {
 public:
 	std::string name;
-	int jobNumber;
-	int operationNumber;
-	int toolNumber;
+	short jobNumber;
+	short operationNumber;
+	short toolNumber;
 	Vector startPosition;
 	Vector endPosition;
 
 	JobOperation();
-	JobOperation(const std::string &name, const int &jobnumber, const int &operationnumber, const int &toolnumber, 
+	JobOperation(const std::string &name, const int &jobnumber, const int &operationnumber, const int &toolnumber,
 		const Vector &startposition, const Vector &endposition);
 
 	bool operator==(const JobOperation &operation) const;
+	bool operator<(const JobOperation &operation) const;
 	void operator=(const JobOperation &operation);
-	
+
 	void print_operation() const;
 };
 
